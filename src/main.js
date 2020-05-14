641def16a9e755ea756ae6a176a56d103b5930d7
swarup <swarup260@gmail.com>
2020-05-14 16:28:59 +0530

    inital setup

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+/* eslint-disable */
+/**
+ * Although is not recommended you can use the full bundle of Vue Material.
+ * This will import ALL components and UI Elements, and will hurt performance:
+ */
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
+Vue.use(VueMaterial)
+// 
 Vue.config.productionTip = false;
 
 new Vue({
