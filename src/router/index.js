9b0c5e87b6c4235a32480bd7815947acf51e62dd
swarup das <swarup260@gmail.com>
2020-09-08 23:54:34 +0530

    routing issue fix and home updated

--- src/router/index.js
+++ src/router/index.js
@@ -21,15 +21,17 @@ Vue.use(VueRouter)
   {
     path: '/signup',
     name: 'SignUp',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue')
   }
 ]
-
+// https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
 const router = new VueRouter({
-  routes
+  mode: 'history', routes: routes
 })
 
 export default router
