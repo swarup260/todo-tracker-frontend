162f25390aba991679e0fa5152928f9600bdda4d
swarup <swarup260@gmail.com>
2020-09-09 12:56:31 +0530

    validation rules added for input

---
+++ src/utils/validation-rule.js
@@ -0,0 +1,15 @@
+const rules = {
+    emailRules : [
+        (value) => !!value || "Required.",
+        (value) => (value || "").length <= 20 || "Max 20 characters",
+        (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }, 
+    ],
+    inputRules : [(value) => !!value || "Required."]
+}
+
+export {
+    rules
+}
