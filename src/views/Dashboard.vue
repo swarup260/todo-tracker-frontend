5d94562bfc6aae5f118cb74d6bc2866eb92da4c6
swarup das <swarup260@gmail.com>
2020-09-10 00:41:52 +0530

    temp todo list view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,17 +4,11 @@
       <v-card>
         <v-card-title>Add New ToDo</v-card-title>
         <v-card-text>
-            <AddTodo />
+          <AddTodo />
         </v-card-text>
-         <v-card-actions>
-            <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-            >
-              Close
-            </v-btn>
-          </v-card-actions>
+        <v-card-actions>
+          <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
     <v-btn
@@ -30,6 +24,14 @@
     >
       <v-icon dark>mdi-plus</v-icon>
     </v-btn>
+    <v-container>
+      <div v-for="todo in todos" :key="todo.id">
+        <v-card class="mx-auto" max-width="344" color="#385F73" dark>
+          <v-card-title class="headline">{{todo.taskName}}</v-card-title>
+          <v-card-subtitle>{{ todo.Description }}</v-card-subtitle>
+        </v-card>
+      </div>
+    </v-container>
   </v-container>
 </template>
 
@@ -43,6 +45,43 @@ export default {
   data() {
     return {
       dialog: false,
+      todos: [
+        {
+          id: 1,
+          taskName: "Programming",
+          Description: "learning C#",
+          status: false,
+          deadline: new Date().getDate(),
+        },
+        {
+          id: 2,
+          taskName: "Programming python",
+          Description: "learning C#",
+          status: true,
+          deadline: new Date().getDate(),
+        },
+        {
+          id: 3,
+          taskName: "Programming Dart",
+          Description: "learning C#",
+          status: false,
+          deadline: new Date().getDate(),
+        },
+        {
+          id: 4,
+          taskName: "Programming Javascript",
+          Description: "learning C#",
+          status: false,
+          deadline: new Date().getDate(),
+        },
+        {
+          id: 5,
+          taskName: "Programming",
+          Description: "learning C#",
+          status: false,
+          deadline: new Date().getDate(),
+        },
+      ],
     };
   },
 };
