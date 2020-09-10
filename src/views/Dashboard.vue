0b9eb4600b0720c7b01a4d4c5e7f3412b23ffac5
swarup <swarup260@gmail.com>
2020-09-10 15:52:23 +0530

    todo list view scaffold added

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -25,12 +25,38 @@
       <v-icon dark>mdi-plus</v-icon>
     </v-btn>
     <v-container>
-      <div v-for="todo in todos" :key="todo.id">
+      <v-row >
+        <v-col cols="12" sm="4" v-for="todo in todos" :key="todo.id">
+        <v-card class="mx-auto" max-width="344" color="#385F73" dark>
+          <v-card-title class="headline">{{todo.taskName}}</v-card-title>
+          <v-card-subtitle>{{ todo.deadline }}</v-card-subtitle>
+          <v-card-text>
+            <div class="text--primary">
+              {{ todo.Description }}
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text>Mark As Complete</v-btn>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+      </v-row>
+      <v-spacer class="mb-16"></v-spacer>
+      <v-row>
+        <h4>Complete Task</h4>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row >
+        <v-col cols="12" sm="4" v-for="todo in todos" :key="todo.id">
         <v-card class="mx-auto" max-width="344" color="#385F73" dark>
           <v-card-title class="headline">{{todo.taskName}}</v-card-title>
           <v-card-subtitle>{{ todo.Description }}</v-card-subtitle>
+          <v-card-actions>
+            <!-- <v-btn text>Set As Complete</v-btn> -->
+          </v-card-actions>
         </v-card>
-      </div>
+        </v-col>
+      </v-row>
     </v-container>
   </v-container>
 </template>
@@ -51,35 +77,35 @@ export default {
           taskName: "Programming",
           Description: "learning C#",
           status: false,
-          deadline: new Date().getDate(),
+          deadline: new Date().toLocaleDateString(),
         },
         {
           id: 2,
           taskName: "Programming python",
           Description: "learning C#",
           status: true,
-          deadline: new Date().getDate(),
+          deadline: new Date().toLocaleDateString(),
         },
         {
           id: 3,
           taskName: "Programming Dart",
           Description: "learning C#",
           status: false,
-          deadline: new Date().getDate(),
+          deadline: new Date().toLocaleDateString(),
         },
         {
           id: 4,
           taskName: "Programming Javascript",
           Description: "learning C#",
           status: false,
-          deadline: new Date().getDate(),
+          deadline: new Date().toLocaleDateString(),
         },
         {
           id: 5,
           taskName: "Programming",
           Description: "learning C#",
           status: false,
-          deadline: new Date().getDate(),
+          deadline: new Date().toLocaleDateString(),
         },
       ],
     };
